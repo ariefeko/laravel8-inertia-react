46d5b1d553106eed2b34688dd7a3aca913207b29
Arief <riff.sense@gmail.com>
2021-01-31 23:02:14 +0700

    adding main customize

---
+++ public/js/resources_js_Pages_home_index_tsx.js
@@ -0,0 +1,20 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_home_index_tsx"],{
+
+/***/ "./resources/js/Pages/home/index.tsx":
+/*!*******************************************!*\
+  !*** ./resources/js/Pages/home/index.tsx ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+
+var Homepage = function Homepage() {
+  return;
+};
+
+/***/ })
+
+}]);
---
+++ resources/js/Pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../../components/common/layout";
+
+const Homepage = () => {
+    return (
+        <Layout title="Selamat datang di websiteku">
+            <div>
+                <p>Dari komponen Homepage</p>
+            </div>
+        </Layout>
+    );
+}
+
+export default Homepage;
